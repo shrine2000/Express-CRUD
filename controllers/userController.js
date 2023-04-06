b80284be059c9ef55f244627c5c607aedbc2deef
Shrine Sabu <shrine02sabu@gmail.com>
2023-04-06 21:21:05 +0530

    minor

--- controllers/userController.js
+++ controllers/userController.js
@@ -49,7 +49,7 @@ const loginUser = asyncHandler(async (req, res) => {
           id: user.id,
         },
       },
-      process.env.ACCESS_TOKEN_SECERT,
+      process.env.ACCESS_TOKEN_SECRET,
       { expiresIn: "15m" }
     );
     res.status(200).json({ accessToken });
--- middleware/validateTokenHandler.js
+++ middleware/validateTokenHandler.js
@@ -3,22 +3,26 @@ const jwt = require("jsonwebtoken");
 
 const validateToken = asyncHandler(async (req, res, next) => {
   let token;
-  let authHeader = req.headers.Authorization || req.header.authorization;
+  let authHeader = req.headers.authorization || req.header.authorization;
   if (authHeader && authHeader.startsWith("Bearer")) {
     token = authHeader.split(" ")[1];
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-      if (err) {
-        res.status(401);
-        throw new Error("User is not authorized");
-      }
-      req.user = decoded.user;
-      next();
-    });
-    if (!token) {
-      res.status(401);
-      throw new Error("User is not authorized/token is missing");
-    }
   }
+
+  if (!token) {
+    res.status(401);
+    throw new Error("User is not authorized/token is missing");
+  }
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.error(err.name + ': ' + err.message); // log the error and its reason
+      res.status(401);
+      throw new Error("User is not authorized");
+    }
+    console.log(decoded);
+    req.user = decoded.user;
+    next();
+  });
 });
 
 module.exports = validateToken;
